0aeec39cfb78910ab00fdb49462d690a4cdf4482
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-26 01:01:30 +0400

    feat(index): Added bcrypt lib

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
+import bcrypt from "bcrypt"; // apahov gaxtnabar unenalu hamar
 
+let users = []; // nerkayis pahel zangvatsi mej , chishty database-um
 const app = express();
 
 app.use(express.json());
@@ -10,9 +12,17 @@ app.get("/register", (req, res) => {
   res.sendFile(path.resolve("pages/register.html"));
 });
 
-app.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("yay");
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hashedPwd = await bcrypt.hash(password, 10);
+
+  users.push({
+    name: name,
+    email: email,
+    password: hashedPwd,
+  });
+  res.redirect("/login");
 });
 
 app.get("/login", (req, res) => {
