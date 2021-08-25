5764d1cd3fae3c6889bcf1227f9693ae02699048
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-08-25 23:56:18 +0400

    feat(pages): Added register and login pages

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve("pages/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("pages/login.html"));
+});
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
 
 app.listen(process.env.PORT);
